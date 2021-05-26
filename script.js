1da64b50de72039d8c0df9616a1d95db9d6ef5f3
RuzbehS <ruzbeh.schafaie@mailbox.org>
2021-05-26 10:35:38 +0200

    added to-do list in script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert(2);
+/* TO-DO:
+    Implement DOM queries for div elements 
+    add event listeners to div elements
+    event listener types should notice the mouse hovering over div elements
+    write functions that switch css of div elements on triggered event
+    add function that resets div elements to default state
+    add function that can change div elements grid size
+    function should be able to update DOM tree (add/remove) with needed div elements    
+*/
